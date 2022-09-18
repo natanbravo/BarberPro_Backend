346fb50541b841a005c8489cdd71b676fbdd3319
natanbravoeasy <NOBprogrammer8@gmail.com>
2022-09-18 20:55:06 -0300

    Feature #320rf6x

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router, Request, Response } from 'express'
 
-const router =Router();
+import { CreateUserController } from './controllers/user/CreateUserController';
 
-router.get('/teste',( req: Request, res: Response) =>{
+const router = Router();
 
-    return res.json({OK: true})
-})
+// router.get('/teste',( req: Request, res: Response) =>{
+
+//     return res.json({OK: true})
+// })
+
+
+// ROTAS USER 
+
+router.post('/users', new CreateUserController().handle)
 
 export {router};
