b492f7e3d49318adcab1754c944eaa2e6228260f
natanbravoeasy <NOBprogrammer8@gmail.com>
2022-09-19 20:51:41 -0300

    Feature #320rf6x

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express'
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
 
 const router = Router();
 
@@ -13,5 +15,7 @@ const router = Router();
 // ROTAS USER 
 
 router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+router.get('/me', new DetailUserController().handle)
 
 export {router};
---
+++ src/services/user/DetailUserService.ts
@@ -0,0 +1,9 @@
+import prismaCLient from "../../prisma";
+
+class UserDetailService{
+    async execute(){
+        return {ok: true}
+    }
+}
+
+export { UserDetailService }
