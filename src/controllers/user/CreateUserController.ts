194f6517587c0aae5067d58bf4a68a2e8a998a44
natanbravoeasy <NOBprogrammer8@gmail.com>
2022-09-18 21:26:00 -0300

    Feature #320rf6x

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -5,7 +5,7 @@ import { CreateUserService } from '../../services/user/CreateUserService'
 class CreateUserController{
     async handle(request: Request, response: Response){
 
-        const {name, email, password} = request.body;
+        const {name, email, password} = request.body;   
 
         const createUserService = new CreateUserService();
 
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,7 @@
+import prismaClient from '../../prisma'
+
+import { hash } from 'bcryptjs'
+
 interface UserRequest{
     name: string
     email: string
@@ -6,9 +10,37 @@ interface UserRequest{
 
 class CreateUserService{
     async execute({name, email, password}: UserRequest){
-        console.log(name)
-        console.log(email)
-        return { ok: true}
+
+        if(!email || email === ''){
+            throw new Error("E-mail incorreto!")
+        }
+        
+         const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+         })
+
+         if(userAlreadyExists){
+            throw new Error("User/E-mail já existe!")
+         }
+
+         const passwordHash = await hash(password, 8);
+
+         const user = await  prismaClient.user.create({
+            data:{
+                name: name,
+                email: email, 
+                password: passwordHash
+            }, 
+            select:{
+                id: true,
+                name: true,
+                email: true
+            }
+         })
+
+         return user;
     }
 }
 
